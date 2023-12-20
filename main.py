ed6fbef388601f1ac5e94adffb3b7d03076f82ee
makharman <125790226+makharman@users.noreply.github.com>
2023-12-20 08:11:18 +0600

    main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ headers = {
     'accept': '*/*',
     'user-agent': 'Mozilla/5.0 (Linux; Android 7.1; Xperia V Build/NDE63X) AppleWebKit/600.3 (KHTML, like Gecko)  Chrome/55.0.2635.298 Mobile Safari/533.5',
 }
-
-url = f'https://www.olx.kz/list/q-%D0%B5%D0%BB%D0%BA%D0%B8/'  
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-cards = soup.find_all("div",class_='css-qfzx1y')
+for i in range(1,4):
+    print(f'Parsing {i} page')
+    url = f'https://www.olx.kz/list/q-%D0%B5%D0%BB%D0%BA%D0%B8/'  
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    cards = soup.find_all("div",class_='css-qfzx1y')
 
 for card in cards:
     card_url = card.a['href']
