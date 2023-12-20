16e5a588e5479f8a3078a68bfcad5f08645f327d
makharman <125790226+makharman@users.noreply.github.com>
2023-12-20 08:35:31 +0600

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-
-headers = {
-    'accept': '*/*',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 7.1; Xperia V Build/NDE63X) AppleWebKit/600.3 (KHTML, like Gecko)  Chrome/55.0.2635.298 Mobile Safari/533.5',
-}
+# HEADER выдает ошибку, без Header код запускается
 for i in range(1,4):
     print(f'Parsing {i} page')
     url = f'https://www.olx.kz/list/q-%D0%B5%D0%BB%D0%BA%D0%B8/?page={i}'  
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
-    cards = soup.find_all("div",class_='css-qfzx1y')
+    cards = soup.find_all("div",class_='css-1sw7q4x')
 
     for card in cards:
         card_url = card.a['href']
-        url = f'https://www.olx.kz{card_url}.html'
+        url = f'https://www.olx.kz{card_url}'
         response = requests.get(url=url)
         soup = BeautifulSoup(response.text,'lxml')
-        description = soup.find('div', 'css-1t507yq.er34gjf0')
+        description = soup.find("div", 'css-1t507yq er34gjf0')
         print(description.text)       
     
